3464be3cf199c98208bf373613aac493926c0ac4
Dan Vicarel <shundra8820@gmail.com>
2022-05-26 02:09:28 -0400

    "main" and "redirect" TLDs are now set from context, not hard-coded to com and net/org

--- cdk/bin/github-pages-website.ts
+++ cdk/bin/github-pages-website.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Duration } from 'aws-cdk-lib';
-import { HostedZone } from 'aws-cdk-lib/aws-route53';
 import { DnssecStack } from '../lib/DnssecStack';
 import { GithubPagesOrganizationWebsiteStack } from '../lib/GithubPagesOrganizationWebsiteStack';
 import { WebsiteRedirectStack } from '../lib/WebsiteRedirectStack';
 
 const app = new cdk.App();
 
-const rootDomain: string = app.node.tryGetContext('rootDomain');
-const comHostedZoneId: string = app.node.tryGetContext('comHostedZoneId');
-const netHostedZoneId: string = app.node.tryGetContext('netHostedZoneId');
-const orgHostedZoneId: string = app.node.tryGetContext('orgHostedZoneId');
+const rootDomain: string = app.node.tryGetContext('mainRootDomain');
+const mainTLD: string = app.node.tryGetContext('mainTLD');
+const mainHostedZoneId: string = app.node.tryGetContext('mainHostedZoneId');
+const redirectTLDs: string = app.node.tryGetContext('redirectTLDs');
+const contextRedirectHostedZoneIds: string = app.node.tryGetContext('redirectHostedZoneIds');
 const logBucketExpirationDays: number | undefined = app.node.tryGetContext('logBucketExpirationDays');
 const githubPagesDefaultDomain: string = app.node.tryGetContext('githubPagesDefaultDomain');
 const githubPagesDnsVerificationDomain: string = app.node.tryGetContext('githubPagesDnsVerificationDomain');
@@ -19,11 +19,11 @@ const githubPagesDnsVerificationTxtValue: string = app.node.tryGetContext('githu
 const githubOrgDnsVerificationDomain: string = app.node.tryGetContext('githubOrgDnsVerificationDomain');
 const githubOrgDnsVerificationTxtValue: string = app.node.tryGetContext('githubOrgDnsVerificationTxtValue');
 
-// Set up DNS records for GitHub Pages website on .com domain with DNSSEC
+// Set up DNS records for GitHub Pages website on main domain with DNSSEC
 const githubPagesOrganizationWebsiteStack = new GithubPagesOrganizationWebsiteStack(app, 'GithubPagesOrganizationWebsiteStack', {
     description: "Resources and DNS settings for hosting the organization website with GitHub Pages",
-    apexDomainName: `${rootDomain}.com`,
-    hostedZoneId: comHostedZoneId,
+    apexDomainName: `${rootDomain}.${rootDomain}`,
+    hostedZoneId: mainHostedZoneId,
     logBucketExpiration: logBucketExpirationDays ? Duration.days(logBucketExpirationDays) : undefined,
     githubPagesDefaultDomain: githubPagesDefaultDomain,
     githubPagesDnsVerificationChallenge: {
@@ -35,27 +35,28 @@ const githubPagesOrganizationWebsiteStack = new GithubPagesOrganizationWebsiteSt
         txtValue: githubOrgDnsVerificationTxtValue,
     },
 });
-new DnssecStack(app, 'ComDnssecStack', {
+new DnssecStack(app, 'MainDnssecStack', {
     description: "DNSSEC settings for the organization website",
-    domainName: `${rootDomain}.com`,
-    hostedZoneId: comHostedZoneId,
+    domainName: `${rootDomain}.${mainTLD}`,
+    hostedZoneId: mainHostedZoneId,
 });
 
-// Set up DNS records to redirect .net domain to main domain, with DNSSEC
-[
-    ".net", ".org"
-].forEach(tld => {
-    new WebsiteRedirectStack(app, 'NetWebsiteRedirectStack', {
-        description: `Resources for redirecting the ${tld} domain to the organization website`,
-        redirectApexDomain: `${rootDomain}.${tld}`,
-        siteDomain: `${rootDomain}.com`,
-        hostedZoneId: netHostedZoneId,
-        logBucket: githubPagesOrganizationWebsiteStack.logBucket,
-    });
+// Set up DNS records to redirect provided domains to the "main" domain, with DNSSEC
+const redirectHostedZoneIds: string[] = contextRedirectHostedZoneIds.split(",");
+redirectTLDs
+    .split(",")
+    .forEach((tld, index) => {
+        new WebsiteRedirectStack(app, `${tld}WebsiteRedirectStack`, {
+            description: `Resources for redirecting the ${tld} domain to the organization website`,
+            redirectApexDomain: `${rootDomain}.${tld}`,
+            siteDomain: `${rootDomain}.${mainTLD}`,
+            hostedZoneId: redirectHostedZoneIds[index],
+            logBucket: githubPagesOrganizationWebsiteStack.logBucket,
+        });
 
-    new DnssecStack(app, 'NetDnssecStack', {
-        description: `DNSSEC settings for the website ${tld} domain`,
-        domainName: `${rootDomain}.${tld}`,
-        hostedZoneId: netHostedZoneId,
+        new DnssecStack(app, `${tld}DnssecStack`, {
+            description: `DNSSEC settings for the website ${tld} domain`,
+            domainName: `${rootDomain}.${tld}`,
+            hostedZoneId: redirectHostedZoneIds[index],
+        });
     });
-});
