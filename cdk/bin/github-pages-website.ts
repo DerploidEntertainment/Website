590bef8589e4a99b0894646376c43232643a5ea7
Dan Vicarel <shundra8820@gmail.com>
2022-05-29 18:46:09 -0400

    Environment-specific data now injected through custom JSON, not CDK context
    - We'll probably only ever deploy from the command-line anyway, so no need to mess with Node.js environment variables
    - The CDK context had no good way of injecting values at synth time other than cumbersome CLI options, so all context values are gone from cdk.json too
    - No longer COPYing a user-specific cdk.json into devcontainer either, devs must copy and fill out (gitignored) env.*.secret.json files instead
    - Actual environment is set at synth time from conventional NODE_ENV env variable, now set in npm scripts
    - Tweaked tsconfig to support importing JSON objects

--- cdk/bin/github-pages-website.ts
+++ cdk/bin/github-pages-website.ts
@@ -5,58 +5,71 @@ import { DnssecStack } from '../lib/DnssecStack';
 import { GithubPagesOrganizationWebsiteStack } from '../lib/GithubPagesOrganizationWebsiteStack';
 import { WebsiteRedirectStack } from '../lib/WebsiteRedirectStack';
 
+import * as sharedEnv from "../env/env.shared.json";
+import * as testEnv from "../env/env.test.json";
+import * as testSecretEnv from "../env/env.test.secret.json";
+import * as prodEnv from "../env/env.prod.json";
+import * as prodSecretEnv from "../env/env.prod.secret.json";
+
 const app = new cdk.App();
 
-const rootDomain: string = app.node.tryGetContext('mainRootDomain');
-const mainTLD: string = app.node.tryGetContext('mainTLD');
-const mainHostedZoneId: string = app.node.tryGetContext('mainHostedZoneId');
-const redirectTLDs: string = app.node.tryGetContext('redirectTLDs');
-const contextRedirectHostedZoneIds: string = app.node.tryGetContext('redirectHostedZoneIds');
-const logBucketExpirationDays: number | undefined = app.node.tryGetContext('logBucketExpirationDays');
-const githubPagesDefaultDomain: string = app.node.tryGetContext('githubPagesDefaultDomain');
-const githubPagesDnsVerificationDomain: string = app.node.tryGetContext('githubPagesDnsVerificationDomain');
-const githubPagesDnsVerificationTxtValue: string = app.node.tryGetContext('githubPagesDnsVerificationTxtValue');
-const githubOrgDnsVerificationDomain: string = app.node.tryGetContext('githubOrgDnsVerificationDomain');
-const githubOrgDnsVerificationTxtValue: string = app.node.tryGetContext('githubOrgDnsVerificationTxtValue');
+const envName: string = process.env.NODE_ENV ?? "test";
+const env = envName === "test"
+    ? {
+        ...sharedEnv,
+        ...testEnv,
+        ...testSecretEnv,
+    }
+    : {
+        ...sharedEnv,
+        ...prodEnv,
+        ...prodSecretEnv,
+    };
+
+env.mainRootDomain = env.mainRootDomain.toLowerCase();
+env.mainTLD = env.mainTLD.toLowerCase();
+env.redirectTLDs = env.redirectTLDs.split(",").map(x => x.toLowerCase()).join(",");
+
 
 // Set up DNS records for GitHub Pages website on main domain with DNSSEC
 const githubPagesOrganizationWebsiteStack = new GithubPagesOrganizationWebsiteStack(app, 'GithubPagesOrganizationWebsiteStack', {
     description: "Resources and DNS settings for hosting the organization website with GitHub Pages",
-    apexDomainName: `${rootDomain}.${rootDomain}`,
-    hostedZoneId: mainHostedZoneId,
-    logBucketExpiration: logBucketExpirationDays ? Duration.days(logBucketExpirationDays) : undefined,
-    githubPagesDefaultDomain: githubPagesDefaultDomain,
+    apexDomainName: `${env.mainRootDomain}.${env.mainTLD}`,
+    hostedZoneId: env.mainHostedZoneId,
+    logBucketExpiration: env.logBucketExpirationDays ? Duration.days(env.logBucketExpirationDays) : undefined,
+    githubPagesDefaultDomain: env.githubPagesDefaultDomain,
     githubPagesDnsVerificationChallenge: {
-        domain: githubPagesDnsVerificationDomain,
-        txtValue: githubPagesDnsVerificationTxtValue,
+        domain: env.githubPagesDnsVerificationDomain,
+        txtValue: env.githubPagesDnsVerificationTxtValue,
     },
     githubOrganizationDnsVerificationChallenge: {
-        domain: githubOrgDnsVerificationDomain,
-        txtValue: githubOrgDnsVerificationTxtValue,
+        domain: env.githubOrgDnsVerificationDomain,
+        txtValue: env.githubOrgDnsVerificationTxtValue,
     },
 });
 new DnssecStack(app, 'MainDnssecStack', {
     description: "DNSSEC settings for the organization website",
-    domainName: `${rootDomain}.${mainTLD}`,
-    hostedZoneId: mainHostedZoneId,
+    domainName: `${env.mainRootDomain}.${env.mainTLD}`,
+    hostedZoneId: env.mainHostedZoneId,
 });
 
 // Set up DNS records to redirect provided domains to the "main" domain, with DNSSEC
-const redirectHostedZoneIds: string[] = contextRedirectHostedZoneIds.split(",");
-redirectTLDs
+const redirectHostedZoneIds: string[] = env.redirectHostedZoneIds.split(",");
+env.redirectTLDs
     .split(",")
     .forEach((tld, index) => {
         new WebsiteRedirectStack(app, `${tld}WebsiteRedirectStack`, {
             description: `Resources for redirecting the ${tld} domain to the organization website`,
             redirectApexDomain: `${rootDomain}.${tld}`,
-            siteDomain: `${rootDomain}.${mainTLD}`,
+            redirectApexDomain: `${env.mainRootDomain}.${tld}`,
+            siteDomain: `${env.mainRootDomain}.${env.mainTLD}`,
             hostedZoneId: redirectHostedZoneIds[index],
             logBucket: githubPagesOrganizationWebsiteStack.logBucket,
         });
 
         new DnssecStack(app, `${tld}DnssecStack`, {
             description: `DNSSEC settings for the website ${tld} domain`,
-            domainName: `${rootDomain}.${tld}`,
+            domainName: `${env.mainRootDomain}.${tld}`,
             hostedZoneId: redirectHostedZoneIds[index],
         });
     });
